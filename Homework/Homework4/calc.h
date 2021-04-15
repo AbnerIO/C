669b25d4ba633e12d0b9a9b223d2a45e6cf341ba
Abner Velázquez <64109505+AbnerIO@users.noreply.github.com>
2021-04-15 09:24:17 -0500

    Support_file: calc.h

--- Homework/Homework4/calc.h
+++ Homework/Homework4/calc.h
@@ -1,1 +1,71 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXOP 100
+#define NUMBER '0'
+double pop(void);
+void push(double);
+int getop(char[]);
+// Todo sobre la pila (pop /sacar, push/meter y gettop/mirar el tope)
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error:stack full, cant push %g\n", f);
+}
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+#include <ctype.h>
+int getch(void);
+void ungetch(int);
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        s[1] = '\0';
+    i = 0;
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c;
+    if (c == '-')
+        if (isdigit(c = getch()) || c == '.')
+            s[++i] = c;
+        else
+        {
+            if (c != EOF)
+                ungetch(c);
+            return '-';
+        }
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            if (c == '.')
+                while (isdigit(s[++i] = c = getch()))
+                    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+};
